3e4ca3d2d4a4d6f29d280e0d20d71dcc7700e6c2
ZiyangWang1 <1270584952@qq.com>
2018-07-06 16:07:28 +0800

    WIP

--- main.c
+++ main.c
@@ -1,20 +1,46 @@
 
-#include <msp430.h>
+#include <MSP430F1232.h>
+#include "configuration.h"
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  P1DIR |= 0x04;  
-  P2DIR |= 0x02;                            // Set P2.1 to output direction
-  P3DIR |= 0x40; 
-  P1OUT &= 0xFB;
-  P3OUT &= 0xBF;
+  // Intialize the I/O registers
+  P1SEL |= P1SEL_INIT;
+  P2SEL |= P2SEL_INIT;
+  P3SEL |= P3SEL_INIT;
+  
+  P1DIR |= P1DIR_INIT;
+  P2DIR |= P2DIR_INIT;
+  P3DIR |= P3DIR_INIT;
+  
+  P1OUT &= ~PX_2_Location;                  // Turn off PWM LED
+  P3OUT &= ~PX_6_Location;                  // Turn off BELL
+  P3OUT |= PX_1_Location|PX_2_Location|PX_3_Location;                   // Turn off RGB
+  
+  P1OUT |= PX_3_Location|PX_4_Location;                 // Turn off all LEDs
+  P2OUT |= PX_1_Location|PX_5_Location;
+  P3OUT |= PX_0_Location;
+  
 
   for (;;)
   {
     volatile unsigned int i;
 
-    P2OUT ^= 0x02;                          // Toggle P2.1 using exclusive-OR
+    P1OUT ^= PX_3_Location;                 // Toggle P1.3 using exclusive-OR
+    P1OUT ^= PX_4_Location;                 // Toggle P1.4 using exclusive-OR
+    P2OUT ^= PX_5_Location;                 // Toggle P2.5 using exclusive-OR
+    
+    if(P1IN & PX_6_Location)
+    {
+      P3OUT &= ~PX_0_Location;
+    }
+    else
+    {
+      P2OUT &= ~PX_1_Location;
+    }
+    
+    
 
     i = 50000;                              // Delay
     do (i--);
---
+++ typedefs.h
@@ -0,0 +1,11 @@
+/*******************************************************************************
+Pin Definitions
+*******************************************************************************/
+
+typedef long s32;
+typedef short s16;
+typedef signed char s8;
+
+typedef unsigned long u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
